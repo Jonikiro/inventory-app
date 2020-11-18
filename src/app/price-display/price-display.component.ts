a9ef31e8c1af26e91c2a524a62d6bcddb03ee606
Jonikiro <mcabrera122@gmail.com>
2020-11-18 14:04:51 -0600

    Final version

---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,11 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'price-display',
+  templateUrl: './price-display.component.html',
+})
+export class PriceDisplayComponent {
+  @Input() price: number;
+
+  constructor() { }
+}
